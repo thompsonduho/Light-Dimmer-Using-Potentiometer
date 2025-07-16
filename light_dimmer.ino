640ef3241ceb996309a8a7036e13cdf1551aee11
thompsonduho <thompsonduho134@gmail.com>
2025-07-16 23:02:16 +0000

    Add files via upload

---
+++ light_dimmer.ino
@@ -0,0 +1,12 @@
+const int potPin = 14;
+
+void setup(){
+  Serial.begin(115200);
+
+}
+void loop(){
+  int potValue = analogRead(potPin);
+  Serial.print("Potentiometer vaue: ");
+  Serial.println(potValue);
+  delay(200);
+}
